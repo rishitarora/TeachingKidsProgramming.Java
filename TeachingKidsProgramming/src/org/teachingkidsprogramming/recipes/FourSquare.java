c2504bf01a17c5680c803215764957d52619d049
Rishit Arora <rishitarora3@rocketmail.com>
2013-06-20 15:39:44 -0700

    Finished HiLow - Rishit/Justin

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/FourSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/FourSquare.java
@@ -7,19 +7,26 @@ public class FourSquare
 {
   public static void main(String[] args)
   {
-    //  Show the tortoise --#1
     Tortoise.show();
-    //  Make the tortoise move as fast as possible --#8
-    //  Do the following 4 times --#9
-    //      drawSquare (recipe below) --#7
-    //      ------------- Recipe for drawSquare --#6
-    //      Do the following 4 times --#5
+    Tortoise.setSpeed(10);
+    drawSquare();
+    drawSquare();
+    drawSquare();
+    drawSquare();
+    Tortoise.turn(90);
+    //  Repeat
+  }
+  private static void drawSquare()
+  {
+    lineTurnright();
+    lineTurnright();
+    lineTurnright();
+    lineTurnright();
+  }
+  private static void lineTurnright()
+  {
     Tortoise.setPenColor(Colors.getRandomColor());
     Tortoise.move(50);
-    //          Turn the tortoise 90 degrees to the right --#4
-    //      Repeat
-    //      ------------- End of drawSquare recipe --#6
-    //      Turn the tortoise 90 degrees to the right --#10
-    //  Repeat
+    Tortoise.turn(90);
   }
 }
--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/HiLow.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/HiLow.java
@@ -1,25 +1,31 @@
 package org.teachingkidsprogramming.recipes;
 
+import java.util.Random;
+
 import org.teachingextensions.logo.utils.Sounds;
 import org.teachingextensions.windows.MessageBox;
-
-import com.spun.util.NumberUtils;
 
 public class HiLow
 {
   public static void main(String[] args)
   {
-    int correctNumber = NumberUtils.getRandomInt(1, 100);
-    for (int i = 1; i <= 8; i++)
+    int correctNumber = new Random(100).nextInt(100);
+    int numberofGuesses = 8;
+    for (int i = 1; i <= numberofGuesses; i++)
     {
-      int guess = MessageBox.askForNumericalInput("Guess a number");
-      if (guess == correctNumber)
+      int chancesLeft = numberofGuesses + 1 - i;
+      int guess = MessageBox.askForNumericalInput("Guess a number (" + chancesLeft + " left)");
+      if (guess < 1 || 100 < guess)
+      {
+        MessageBox.showMessage("Your number  " + guess + " is not between 1 and 100");
+      }
+      else if (guess == correctNumber)
       {
         Sounds.playBeep();
         MessageBox.showMessage("You have won the game");
         System.exit(0);
       }
-      else if (guess >= correctNumber)
+      else if (correctNumber < guess)
       {
         MessageBox.showMessage("The number is too high");
       }
@@ -28,6 +34,6 @@ public class HiLow
         MessageBox.showMessage("The number is too low");
       }
     }
-    MessageBox.showMessage("You have lost the game");
+    MessageBox.showMessage("You have lost the game. The correct number was " + correctNumber);
   }
 }
