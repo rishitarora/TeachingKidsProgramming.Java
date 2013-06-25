ce5ac016edefb88f7438807b7126691e4897b37e
Rishit Arora <rishitarora3@rocketmail.com>
2013-06-25 01:52:40 -0700

    Finished - Rishit

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/DigiFlower.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/DigiFlower.java
@@ -11,35 +11,78 @@ public class DigiFlower
     Tortoise.show();
     Tortoise.setSpeed(10);
     //    Make the background silver --#8
-    //    Make the line the tortoise draws 3 pixels wide --#20
-    //    createColorPalette (recipe below) --#9
-    //    Do the following 15 times --#19
-    //      drawOctogon (recipe below) --#10
-    //      Turn the tortoise 1/15th of 360 degrees to the right --#18
-    //    Repeat
-    //    ------------- Recipe for createColorPalette --#9
-    //    Color 1 is red --#3
-    //    Color 2 is dark orange --#11
-    //    Color 3 is gold --#12
-    //    Color 4 is yellow --#13
-    ColorWheel.addColor(Colors.Reds.Red);
-    //    Add color 2 to the color wheel --#11.1
-    //    Add color 3 to the color wheel --#12.1
-    //    Add color 4 to the color wheel --#13.1
-    //    Add color 4 to the color wheel --#14
-    //    Add color 3 to the color wheel --#15
-    //    Add color 2 to the color wheel --#16
-    //    Add color 1 to the color wheel --#17
-    //    ------------- Recipe for drawOctogon --#10
-    myRefactor();
-    myRefactor();
-    myRefactor();
-    myRefactor();
-    myRefactor();
-    myRefactor();
-    myRefactor();
-    myRefactor();
+    Tortoise.setPenWidth(3);
+    fifteenTimes();
     //    Repeat  
+  }
+  private static void fifteenTimes()
+  {
+    Tortoise.turn(360 / 5);
+    createColorPallete();
+    drawOctagon();
+    Tortoise.turn(360 / 5);
+    createColorPallete();
+    drawOctagon();
+    Tortoise.turn(360 / 5);
+    createColorPallete();
+    drawOctagon();
+    Tortoise.turn(360 / 5);
+    createColorPallete();
+    drawOctagon();
+    Tortoise.turn(360 / 5);
+    createColorPallete();
+    drawOctagon();
+    Tortoise.turn(360 / 5);
+    createColorPallete();
+    drawOctagon();
+    Tortoise.turn(360 / 5);
+    createColorPallete();
+    drawOctagon();
+    Tortoise.turn(360 / 5);
+    createColorPallete();
+    drawOctagon();
+    Tortoise.turn(360 / 5);
+    createColorPallete();
+    drawOctagon();
+    Tortoise.turn(360 / 5);
+    createColorPallete();
+    drawOctagon();
+    Tortoise.turn(360 / 5);
+    createColorPallete();
+    drawOctagon();
+    Tortoise.turn(360 / 5);
+    createColorPallete();
+    drawOctagon();
+    Tortoise.turn(360 / 5);
+    createColorPallete();
+    drawOctagon();
+    Tortoise.turn(360 / 5);
+    createColorPallete();
+    drawOctagon();
+    Tortoise.turn(360 / 5);
+    createColorPallete();
+    drawOctagon();
+  }
+  private static void createColorPallete()
+  {
+    ColorWheel.addColor(Colors.Oranges.DarkOrange);
+    ColorWheel.addColor(Colors.Yellows.Gold);
+    ColorWheel.addColor(Colors.Yellows.Yellow);
+    ColorWheel.addColor(Colors.Yellows.Yellow);
+    ColorWheel.addColor(Colors.Yellows.Gold);
+    ColorWheel.addColor(Colors.Oranges.DarkOrange);
+    ColorWheel.addColor(Colors.Reds.Red);
+  }
+  private static void drawOctagon()
+  {
+    myRefactor();
+    myRefactor();
+    myRefactor();
+    myRefactor();
+    myRefactor();
+    myRefactor();
+    myRefactor();
+    myRefactor();
   }
   private static void myRefactor()
   {
